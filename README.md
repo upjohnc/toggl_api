7ed1c6b6c4b7e8b54f6957fb896866b2bdd94cca
Chad Upjohn <upjohnc@gmail.com>
2017-08-20 08:23:13 -0500

    testing update version

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
-### Pip Install
+# Pip Install
 `pip install git+git://github.com/upjohnc/toggl_api.git`
+
+### update:
+ `python setup.py sdist`
--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='TogglApi',
-    version='0.1dev',
+    version='0.2',
     packages=['toggl_api', ],
     license='Creative Commons Attribution-Noncommercial-Share Alike license',
     long_description=open('README.md').read(),
